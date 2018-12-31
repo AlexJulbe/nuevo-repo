783cbc494b401ba7afc30ad6fa51edb2ca8f0b8f
Antxon <agoca80@gmail.com>
2018-12-31 11:19:28 +0100

    iterate through vapps

--- main.py
+++ main.py
@@ -2,17 +2,20 @@
 
 import json
 
-from abiquo.client import Abiquo    
+from abiquo.client import Abiquo
 from abiquo.client import check_response
 
 api = Abiquo("https://mothership.bcn.abiquo.com/api", auth=("ajulbe", "julbe1991"))
 code, vdcs = api.cloud.virtualdatacenters.get(headers={
     'accept':'application/vnd.abiquo.virtualdatacenters+json'
 })
-code2, vapp = api.cloud.virtualappliances.get(headers={
-    'accept' : 'application/vnd.abiquo.virtualappliances+json'
-})
-    
 check_response(200, code, vdcs)
 print ("Response code vdcs: %s" % code)
-print ("Response code vapps: %s" % code2)
+
+for vdc in vdcs:
+    code, vapps = vdc.follow('virtualappliances').get(headers={
+        'accept' : 'application/vnd.abiquo.virtualappliances+json'
+    })
+    check_response(200, code, vapps)
+    for vapp in vapps:
+        print vapp.name
