82cdf289516095d52c664013fadcb966c4a7771a
AlexJulbe <alex.julbe@abiquo.com>
2019-01-02 12:40:39 +0100

    Add VM

--- main.py
+++ main.py
@@ -2,17 +2,27 @@
 
 import json
 
-from abiquo.client import Abiquo    
+from abiquo.client import Abiquo
 from abiquo.client import check_response
 
 api = Abiquo("https://mothership.bcn.abiquo.com/api", auth=("ajulbe", "julbe1991"))
 code, vdcs = api.cloud.virtualdatacenters.get(headers={
     'accept':'application/vnd.abiquo.virtualdatacenters+json'
 })
-code2, vapp = api.cloud.virtualappliances.get(headers={
-    'accept' : 'application/vnd.abiquo.virtualappliances+json'
-})
-    
 check_response(200, code, vdcs)
-print ("Response code vdcs: %s" % code)
-print ("Response code vapps: %s" % code2)
+"""print ("Response code vdcs: %s" % code)"""
+
+for vdc in vdcs:
+    code, vapps = vdc.follow('virtualappliances').get(headers={
+        'accept' : 'application/vnd.abiquo.virtualappliances+json'
+    })
+    check_response(200, code, vapps)
+    print vdc.name
+    for vapp in vapps:
+        code, vms = vapp.follow('virtualmachines').get(headers={
+        'accept' : 'application/vnd.abiquo.virtualmachines+json'
+        })
+        check_response(200, code, vms)
+        print vapp.name
+        for vm in vms:
+            print vm.name
